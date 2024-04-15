dadbb39086a32caf7d5d60883cf456bfce51866d
Piotrek <138370309+Shunii06@users.noreply.github.com>
2024-04-15 11:25:25 +0200

    added promises hw eazy

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+document.querySelector('.form').addEventListener('submit', function (ev) {
+  ev.preventDefault();
+
+  const delay = parseInt(this.elements.delay.value);
+  const step = parseInt(this.elements.step.value);
+  const amount = parseInt(this.elements.amount.value);
+
+  let currentDelay = delay;
+  for (let i = 0; i < amount; i++) {
+    createPromise(i + 1, currentDelay)
+      .then(result => {
+        Notiflix.Notify.success(
+          `Promise ${result.position} resolved after ${result.delay}ms`
+        );
+      })
+      .catch(error => {
+        Notiflix.Notify.failure(
+          `Promise ${error.position} rejected after ${error.delay}ms`
+        );
+      });
+
+    currentDelay += step;
+  }
+});
